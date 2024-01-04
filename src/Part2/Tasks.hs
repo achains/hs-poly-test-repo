848d07fe53fe69beab25232609e75d1e55853689
achains <arthur.salio@gmail.com>
2024-01-04 17:57:35 +0300

    feat: fix and complete task 2

--- src/Part2/Tasks.hs
+++ src/Part2/Tasks.hs
@@ -26,15 +26,25 @@ infixl 8 |*|
 -- Заменить переменную `varName` на `replacement`
 -- во всём выражении `expression`
 replaceVar :: String -> Term -> Term -> Term
-replaceVar varName replacement expression = notImplementedYet
+replaceVar varName replacement (IntConstant x) = IntConstant x 
+replaceVar varName replacement (Variable name) | name == varName = replacement
+                                               | otherwise       = Variable name 
+replaceVar varName replacement (BinaryTerm op lhs rhs) = 
+   let lhs' = replaceVar varName replacement lhs in 
+      let rhs' = replaceVar varName replacement rhs in 
+         BinaryTerm op lhs' rhs'                                         
+
 
 -- Посчитать значение выражения `Term`
 -- если оно состоит только из констант
 evaluate :: Term -> Term
-evaluate expr = case expr of
-   IntConstant{intValue=x} -> IntConstant{intValue=x}
-   BinaryTerm{op=op, lhv=lhv, rhv=rhv} -> case op of 
-      Plus -> lhv |+| rhv
-      Minus -> lhv |-| rhv 
-      Times -> lhv |*| rhv
-   otherwise -> IntConstant(1)
+evaluate (BinaryTerm op lhs rhs) = 
+   let lhs' = evaluate lhs in 
+      let rhs' = evaluate rhs in 
+         case (lhs', rhs') of 
+            (IntConstant lhv, IntConstant rhv) -> case op of 
+               Plus -> IntConstant (lhv + rhv)
+               Minus -> IntConstant (lhv - rhv)
+               Times -> IntConstant (lhv * rhv)
+            _ -> BinaryTerm op lhs' rhs'
+evaluate expression = expression
