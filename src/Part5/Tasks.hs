9ead6087501b102a2f0dcdc574afa0b01c488a62
achains <arthur.salio@gmail.com>
2024-01-04 23:38:17 +0300

    feat: part 5 full

--- src/Part5/Tasks.hs
+++ src/Part5/Tasks.hs
@@ -4,29 +4,47 @@ import Util(notImplementedYet)
 
 -- Реализуйте левую свёртку
 myFoldl :: (b -> a -> b) -> b -> [a] -> b
-myFoldl = notImplementedYet
+myFoldl f acc [] = acc
+myFoldl f acc (x:xs) = myFoldl f (f acc x) xs 
+
 
 -- Реализуйте правую свёртку
 myFoldr :: (a -> b -> b) -> b -> [a] -> b
-myFoldr = notImplementedYet
+myFoldr f acc [] = acc
+myFoldr f acc (x:xs) = f x (myFoldr f acc xs) 
 
 -- Используя реализации свёрток выше, реализуйте все остальные функции в данном файле
 
 myMap :: (a -> b) -> [a] -> [b]
-myMap f = notImplementedYet
+myMap f = 
+    let helper x acc = f x : acc 
+    in myFoldr helper []
 
 myConcatMap :: (a -> [b]) -> [a] -> [b]
-myConcatMap f = notImplementedYet
+myConcatMap f = 
+    let helper x acc = f x ++ acc
+    in myFoldr helper [] 
 
 myConcat :: [[a]] -> [a]
-myConcat = notImplementedYet
+myConcat = 
+    let helper x acc = myFoldr (:) acc x
+    in myFoldr helper []
 
 myReverse :: [a] -> [a]
-myReverse = notImplementedYet
+myReverse = 
+    let helper x acc = acc : x 
+    in myFoldl helper []
 
 myFilter :: (a -> Bool) -> [a] -> [a]
-myFilter p = notImplementedYet
+myFilter p = 
+    let helper x acc | p x = x : acc
+                     | otherwise = acc
+    in myFoldr helper []
 
 myPartition :: (a -> Bool) -> [a] -> ([a], [a])
-myPartition p = notImplementedYet
+myPartition p = 
+    let helper x (tacc, facc) | p x       = (x : tacc, facc)
+                              | otherwise = (tacc, x : facc)
+    in myFoldr helper ([], [])
+
 
